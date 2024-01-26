ba8da2269189a711e1854157ed0b77d695eb826b
Shady-Mahmoud9 <shadyabkader909@gmail.com>
2024-01-26 15:50:34 +0200

    w

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -46,7 +46,7 @@ function NavBar() {
               </div>
               <div className="hidden sm:ml-6 sm:block">
                 <div className="flex space-x-4">
-                  <NavLink to={'/login'} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</NavLink>
+                  <NavLink to={'/'} className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</NavLink>
                   <NavLink to={'/login'} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">category</NavLink>
                   <NavLink to={'/login'} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Products</NavLink>
                   <NavLink to={'/login'} className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">review</NavLink>
